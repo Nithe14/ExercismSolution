a752f777e7c2f67dbf5ad0b180292bf2ba9d50d0
nithe14 <imnithe@gmail.com>
2023-04-17 10:51:12 +0200

    add anagram solution

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,19 +1,15 @@
-//TODO!
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let sorted_word = unstable_sort_string(word.to_lowercase().chars().collect());
+    let mut sorted_word = word.to_lowercase().chars().collect::<Vec<char>>();
+    sorted_word.sort_unstable();
     possible_anagrams
         .iter()
-        .filter(|item| unstable_sort_string(item.to_lowercase().chars().collect()) == sorted_word)
-        .filter(|item| item.to_lowercase() != word.to_lowercase())
+        .filter(|item| {
+            let mut sorted_item = item.to_lowercase().chars().collect::<Vec<char>>();
+            sorted_item.sort_unstable();
+            sorted_word == sorted_item && word.to_lowercase() != item.to_lowercase()
+        })
         .copied()
         .collect()
 }
-
-fn unstable_sort_string(mut vec: Vec<char>) -> Vec<char> {
-    vec.sort_unstable();
-    vec
-}
-
-//TODO!
