751702cfe7bf4906061cf69c6cbd92e2fc0a3c8f
nithe14 <imnithe@gmail.com>
2023-04-21 13:30:40 +0200

    add parallel but slow af

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,27 +1,50 @@
 //TODO!
-//not multiworkers
+//parallel but slower then seq xD
+use std::cmp::max;
 use std::collections::HashMap;
+use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let mut char_map = HashMap::new();
+    if input.is_empty() {
+        return char_map;
+    }
 
-    input.iter().for_each(|line| {
-        line.chars().for_each(|c| {
-            if c.is_alphabetic() {
-                let lower_c = c.to_lowercase().to_string().chars().nth(0).unwrap();
-                *char_map.entry(lower_c).or_insert(0) += 1;
-            }
+    let chars: Vec<char> = input.join("").chars().collect::<Vec<char>>();
+    let chunk_size = max(chars.len() / worker_count, 1);
+    let chunks: Vec<Vec<char>> = chars
+        .chunks(chunk_size)
+        .map(|chunk| chunk.to_vec())
+        .collect();
+    let mut handles = Vec::with_capacity(chunks.len());
+
+    for chunk in chunks {
+        let handle = thread::spawn(move || {
+            let map = count_in_chunk(chunk);
+            map
         });
-    });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        let map = handle.join().unwrap();
+        for (k, v) in map {
+            *char_map.entry(k).or_insert(0) += v
+        }
+    }
 
     char_map
-
-    //unimplemented!(
-    //"Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
-    //match worker_count {
-    //1 => "1 worker".to_string(),
-    //_ => format!("{worker_count} workers"),
-    //}
-    //);
 }
+
+fn count_in_chunk(input: Vec<char>) -> HashMap<char, usize> {
+    let mut map = HashMap::new();
+    input.iter().for_each(|c| {
+        if c.is_alphabetic() {
+            let lower_c = c.to_lowercase().to_string().chars().nth(0).unwrap();
+            *map.entry(lower_c).or_insert(0) += 1;
+        }
+    });
+    map
+}
+
 //TODO!
