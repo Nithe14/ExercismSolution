443a4d4fe317a93d3a7c6ef9e91ca1d4368ef516
nithe14 <imnithe@gmail.com>
2023-06-22 10:47:12 +0200

    complete allergies

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,8 +1,9 @@
+use Allergen::*;
 pub struct Allergies {
     allergens: Vec<Allergen>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Allergen {
     Eggs,
     Peanuts,
@@ -15,52 +16,40 @@ pub enum Allergen {
 }
 
 impl Allergies {
+    fn push(&mut self, value: u32) {
+        match value {
+            1 => self.allergens.push(Eggs),
+            2 => self.allergens.push(Peanuts),
+            4 => self.allergens.push(Shellfish),
+            8 => self.allergens.push(Strawberries),
+            16 => self.allergens.push(Tomatoes),
+            32 => self.allergens.push(Chocolate),
+            64 => self.allergens.push(Pollen),
+            128 => self.allergens.push(Cats),
+            _ => (),
+        }
+    }
     pub fn new(score: u32) -> Self {
         let allergens_nums = vec![1, 2, 4, 8, 16, 32, 64, 128];
-        let mut allergens = Vec::new();
+        let mut allergens = Allergies {
+            allergens: Vec::new(),
+        };
 
-        push_summing_numbers(allergens_nums, score, &mut allergens);
-        let allergens = allergens
-            .iter()
-            .map(|a| match a {
-                1 => Allergen::Eggs,
-                2 => Allergen::Peanuts,
-                4 => Allergen::Shellfish,
-                8 => Allergen::Strawberries,
-                16 => Allergen::Tomatoes,
-                32 => Allergen::Chocolate,
-                64 => Allergen::Pollen,
-                _ => Allergen::Cats,
-            })
-            .collect::<Vec<Allergen>>();
-        Allergies {
-            allergens: allergens,
+        let mut sum = score % 256; //modulo works on >255 numbers
+        for num in allergens_nums.iter().rev() {
+            if *num <= sum {
+                sum -= num;
+                allergens.push(*num);
+            }
         }
+        allergens
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
         self.allergens.contains(allergen)
     }
 
-    pub fn allergies(self) -> Vec<Allergen> {
-        self.allergens
+    pub fn allergies(&self) -> Vec<Allergen> {
+        self.allergens.clone()
     }
 }
-
-fn push_summing_numbers(set: Vec<u32>, sum: u32, final_vec: &mut Vec<u32>) -> () {
-    let mut tmp = sum % 256; //modulo works on >255 numbers
-    for num in set.iter().rev() {
-        if *num <= tmp {
-            tmp -= num;
-            final_vec.push(*num);
-        }
-        if final_vec.iter().sum::<u32>() == sum % 256 {
-            break;
-        }
-    }
-    if final_vec.iter().sum::<u32>() != sum % 256 {
-        final_vec.clear();
-    }
-}
-//TO DO!
-//have to refactor this code
